0edebb54833d67540143e2140e8f6dbb88544013
Lina_Li <ilyinadother@gmail.com>
2021-02-03 21:24:30 +0200

    first commit

---
+++ лаб5.cpp
@@ -0,0 +1,15 @@
+﻿//Элементы массива заданы случайным образом. Упорядочить столбцы, не содержащие нулевых значений, по возрастанию.
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num;
+	cout << "Vvedite razmer massiva: ";
+	cin >> num;
+	int* p_darr = new int[num];
+	for (int i = 0; i < num; i++);
+	{
+		p_darr[i] = i;
+
+	}
+}
